4f91b969fb366e03fa480be3f0b0a0220199edd1
Bahadır Kayış <bahadir.kayis24@gmail.com>
2021-11-26 16:45:49 +0300

    push

--- Kredi Kartı Takip/Form1.Designer.cs
+++ Kredi Kartı Takip/Form1.Designer.cs
@@ -57,7 +57,7 @@ namespace Kredi_Kartı_Takip
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(48, 30);
+            this.button1.Location = new System.Drawing.Point(40, 50);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
